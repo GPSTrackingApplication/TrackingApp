93cd06d99acc3662bb7a3a5c016cae014040a873
struanxyz <40318347@live.napier.ac.uk>
2021-04-13 14:35:15 +0100

    Add project files.

---
+++ TrackingApp/TrackingApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TrackingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void StartTrackingBtn_Clicked(object sender, EventArgs e)
+        {
+            StartTrackingBtn.IsVisible = false;
+            StopTrackingBtn.IsVisible = true;
+
+
+        }
+
+        private void StopTrackingBtn_Clicked(object sender, EventArgs e)
+        {
+            StopTrackingBtn.IsVisible = false;
+            StartTrackingBtn.IsVisible = true;
+        }
+    }
+}
