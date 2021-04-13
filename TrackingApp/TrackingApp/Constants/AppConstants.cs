cd2aa2b3fe24409992ab2628d665537633f66bcb
struanxyz <40318347@live.napier.ac.uk>
2021-04-13 16:12:12 +0100

    Adds google maps functionality

--- TrackingApp/TrackingApp/Constants/AppConstants.cs
+++ TrackingApp/TrackingApp/Constants/AppConstants.cs
@@ -6,6 +6,6 @@ namespace TrackingApp.Constants
 {
     public class AppConstants
     {
-        public const string GoogleMapsApiKey = "";
+        public const string GoogleMapsApiKey = "AIzaSyAIYcuxtJycDlVtuv2B-12-DspLzCSauV4";
     }
 }
